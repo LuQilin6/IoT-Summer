110ec48e2ae7370b0afee3675602a78a6182df6a
LuQilin6 <65828926+LuQilin6@users.noreply.github.com>
2020-07-16 12:20:54 -0400

    Created LDR based project

---
+++ analogValue/analogValue.ino
@@ -0,0 +1,25 @@
+int ldr_pin = A0;
+int analogValue = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogValue = analogRead(ldr_pin);
+  Serial.println(analogValue);
+  if(analogValue<=400 && analogValue>=0){
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+  }else if(analogValue>400 && analogValue<=700){
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+  }else if(analogValue>700 && analogValue<=1024){
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+  }
+  delay(50);
+}
